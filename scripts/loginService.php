1fad281f7d2374dc7659abb8a97e8f3ae0717df2
Adeeb <ttc_rulz@hotmail.com>
2011-06-13 21:50:39 -0700

    Edited scripts/loginService.php via GitHub

--- scripts/loginService.php
+++ scripts/loginService.php
@@ -1,6 +1,9 @@
 <?php
 
 #Note: Left this info out as this is a public repository. Use the same login script that is on my webserver, for testing. (It is protected there, with the values filled out)
+
+#Note 2: Don't worry about the change history showing the old password. The login service details have been changed directly in the database, so the old details in history should not work. 
+
 $connection = "";
 $username="";
 $password=""; 
