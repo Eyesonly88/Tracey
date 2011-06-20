ccb80b97f23033fd18893fffdb264897e617ddc7
Adeeb <trah012@aucklanduni.ac.nz>
2011-06-21 03:37:04 +1200

    syntax

---
+++ scripts/sandbox.php
@@ -0,0 +1,25 @@
+<?php 
+
+# Test out functions by including them and executing here.
+
+include 'scripts/includes/sanitize.php';
+include 'scripts/includes/functions.php';
+include 'scripts/includes/headers.php';
+include 'scripts/includes/footers.php';
+include 'scripts/includes/formfunctions.php';
+include 'scripts/includes/sql_connect.php' ;
+include 'scripts/includes/sql_userfunctions.php' ;
+include 'scripts/includes/sql_projectfunctions.php';
+include 'scripts/includes/sql_other.php';
+include 'scripts/includes/sql_checks.php';
+include 'scripts/includes/sql_prepared.php';  
+
+
+
+
+
+
+
+
+
+?>
