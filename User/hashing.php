6d31ceea0b95a1c5eb4fdbc35946c191b80c7e4a
Mohammed <eyesonly88@gmail.com>
2011-06-22 16:47:38 +1200

    hashing functions and login
    Signed-off-by: Mohammed <eyesonly88@gmail.com>

--- User/hashing.php
+++ User/hashing.php
@@ -1,5 +1,4 @@
 <?php
-// i will edit this later and refactor it
 function myHash($password){
 	
 	$uniqueSalt = uniqueSalt();
@@ -14,8 +13,7 @@ function myCrypt($hash){
 
 	// hash it 1000 times so it takes more time for attackers
 	// to build a rainbow table
-	for ($i = 0; $i < 1000; $i++;)
-	{
+	for ($i = 0; $i < 1000; $i++){
 		$hash = sha1($hash);
 	}
 	return $hash;
@@ -24,25 +22,37 @@ function myCrypt($hash){
 
 function uniqueSalt(){
 
-	return substr(sha1a(mt_rand()), 0, 22);
+	return substr(sha1(mt_rand()), 0, 22);
 
 }
 
-function checkPass($password){
+
+
+function checkPass($password, $email){
 	
 	// get the uniqueSalt from user record
+	
 	$hash = sha1($uniqueSalt . $password);
 	$hashed = myCrypt($hash);
 	
 	// get the hased password from user record
 	
 	// compare $hashed with the hashed password in user record
-	
-	
-	
 
 }
 
-myHash();
-
+function get_user_salt($email){
+	global $connection;
+	$query = $connection->stmt_init();
+	$sql_stmnt = "SELECT Salt FROM User WHERE Email = ?";
+	if ($query->prepare($sql_stmnt)) {
+		
+		$query -> bind_param("s", $em);
+		$em = $email;		
+		$results = dynamicBindResults($query);
+		print_r($results);
+		
+	}	
+	
+}
 ?>
