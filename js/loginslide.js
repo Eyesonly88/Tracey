5bdd77934ebd747bd9c3cd05636bd017f80c3a4f
Adeeb <trah012@aucklanduni.ac.nz>
2011-06-27 21:41:33 +1200

    Experimenting with more jquery by adding a hide/show animation thing for the openID div. (Not practical, but just experimenting :-) )

--- js/loginslide.js
+++ js/loginslide.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+	$("#openid_form").hide();
 	
+	var openidshowing = 0;
 	// Expand Panel
 	$("#open").click(function(){
 		$("div#panel").slideDown("slow");
@@ -14,6 +16,16 @@ $(document).ready(function() {
 	// Switch buttons from "Log In | Register" to "Close Panel" on click
 	$("#toggle a").click(function () {
 		$("#toggle a").toggle();
+	});	
+	
+	$("#showopenid").click(function () {
+		if (openidshowing == 0) { 
+			$("#openid_form").show("slow");
+			openidshowing = 1;
+		} else { 
+			$("#openid_form").hide("slow");
+			openidshowing = 0;
+		}
 	});		
 		
 });
