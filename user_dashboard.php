bb1d92a9428c6fd499fd62a02917e856cbfb5b2f
Adeeb <trah012@aucklanduni.ac.nz>
2011-06-28 20:59:08 +1200

    Minor

--- user_dashboard.php
+++ user_dashboard.php
@@ -1,18 +1,6 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sessions.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sessions.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_userfunctions.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_connect.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sanitize.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/functions.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/headers.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/footers.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/formfunctions.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_projectfunctions.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_issuefunctions.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_other.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_checks.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_prepared.php');
+
 // Confirm if user is logged in.
 confirmLogin();
 
