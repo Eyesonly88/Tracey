320a6d4365a26bd042416ffae7fcb3736dc714f9
Adeeb <trah012@aucklanduni.ac.nz>
2011-07-04 16:29:46 +1200

    Implementing more project functions.

--- scripts/includes/sql_projectfunctions.php
+++ scripts/includes/sql_projectfunctions.php
@@ -22,19 +22,16 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_prepared.php');
 		$query = $connection->stmt_init();
 		
 		
-		$sql_createProject = "INSERT INTO Project(ProjectName, ProjectLeader) VALUES(?, ?)";
-		
+		$sql_createProject = "INSERT INTO Project(ProjectName, ProjectLeader) VALUES(?, ?)";	
 		if ($query->prepare($sql_createProject)) {
 			
 			$query->bind_param("sd", $pname, $pleader);
 			$pname = $name;
 			$pleader = $user;
 			$query->execute();
-			return "1";
-			
+			return "1";		
 		}
-		return "0";
-		
+		return "0";	
 	}
 
 	function deleteProjectById($id) {
@@ -46,10 +43,8 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_prepared.php');
 			
 			$query->bind_param("i", $pid);
 			$pid = $id;		
-			$query->execute();	
-			
+			$query->execute();			
 		}
-	
 	}
 
 
@@ -59,49 +54,72 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_prepared.php');
 		$sql_modifyProject = ""; 
 		
 		if ($query->prepare($sql_modifyProject))  {
-			
-			
+					
 		}
 	
 	}
 	
-	
-	function checkProjectExists($project) {
+	/* Checks if a project with $projectname exists for the user with $userid */
+	function checkProjectExistsByName($projectname, $userid) {
 		global $connection;
 		$query = $connection->stmt_init();
-		$sql_getProjectInfo = "";
+		$results = '';
+		$sql_getProjectInfo = "SELECT p.ProjectId, p.ProjectName FROM Project p INNER JOIN User u ON u.UserId = p.ProjectLeader WHERE p.ProjectName=? AND u.UserId=?";
+		$query->prepare($sql_getProjectInfo);
+		$query->bind_param("ss", $name, $uid);
+		$name = $projectname;
+		$uid = $userid;
 		
-		if ($query->prepare($sql_getProjectInfo)) {
-					
-		}	 
+		$results = dynamicBindResults($query);
+		
+		if (empty($results)) {
+			return '';			
+		}
+		
+		return $results;
 	}
 	
 	
-	
-	function changeProjectStatus() {
+	/* Updates the status of the selected project ($projectid) with the status that corresponds to $statusid */
+	function changeProjectStatus($projectid, $statusid) {
 		global $connection;
 		$query = $connection->stmt_init(); 
 		$sql_changeProjectStatus = "";
 		
-		if ($query->prepare($sql_changeProjectStatus)) {
-			
+		if ($query->prepare($sql_changeProjectStatus)) {		
 		}
-	
 	}
 	
+	
+	/* Gets the list of projects created by the user with $userid */
 	function getProjectsByUserId($userid) {
 		
+		global $connection;
+		$userinfo = array();
+		$results = '';
+		$query = $connection -> stmt_init();
 		
+		$sql_getProjects = "SELECT p.ProjectId, p.ProjectName, p.ProjectType, p.ProjectLeader from Project p INNER JOIN User u ON u.UserId = p.ProjectLeader WHERE u.UserId=?";
+		$query->prepare($sql_getProjects);
+		$query -> bind_param("s", $id);
+		$id = $userid;
+		$results = dynamicBindResults($query);
+		
+		if (empty($results)) {
+			return '';
+		}
+		$userinfo = $results;
+		return $userinfo;	
 	}
 	
+	/* Gets the projects created by the user with email $email */
 	function getProjectsByEmail($email) {
 		
 		global $connection;
 		$emailExists = 0;
 		$userinfo = array();
-		$results = NULL;
-		$query = $connection ->stmt_init();
-		
+		$results = '';
+		$query = $connection ->stmt_init();	
 		$sql_getProjects = "SELECT p.ProjectId, p.ProjectName, p.ProjectType, p.ProjectLeader from Project p INNER JOIN User u ON u.UserId = p.ProjectLeader WHERE Email=?";	
 		$query->prepare($sql_getProjects);
 		$query -> bind_param("s", $em);
@@ -109,13 +127,23 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_prepared.php');
 		$results = dynamicBindResults($query);	
 				
 		if (empty($results)) { 	
-			return "";
+			return '';
 		}
-
+		
 		$userinfo = $results;		
 		return $userinfo;
 		
 	}
+	
+	function addUserToProject($projectid, $useremail) {
+			
+		global $connection;
+		$query = $connection->stmt_init();	
+		$sql_addUserToProject = "";
+			
+		
+	}
+	
 
 	function getWatchedProjectsByUserEmail($email) {
 		
--- scripts/sandbox.php
+++ scripts/sandbox.php
@@ -19,72 +19,32 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_prepared.php');
 	//$email = "ttc_rulz@hotmail.com";
 	//$result = createProject("test33", $email);	
 	
-	echo $result;
+	
 	
 	global $connection;
+	$results = '';
+	$projectname = 'Test1';
+	$response = '';
+	$results = checkProjectExistsByName($projectname);	
+	if (empty($results)) { return 'No project with name ' . $projectname . ' found.'; } else { echo 'Project found <BR />'; } 
 	
-	echo "Initiating connection object... <BR />";
-	//echo $connection;
-	$userinfo = array();
-	$query = $connection->stmt_init();
-	$sql_getUser = "SELECT * FROM User WHERE UserId = ?";	
-	echo "Preparing.. <BR />";
-	$query->prepare($sql_getUser);	
-	echo "Preparing..Success <BR />";
-	echo "Binding params... <BR />";
-	$query->bind_param("i", $userid);
-	echo "Binding params...Success <BR />";
-	$userid = $id;
-	$results = dynamicBindResults($query);
-	if (empty($results)) { return ""; }
-	$userinfo = $results[0];		
+	foreach($results as $row){
+			$response = $response . '<tr> ';
+			$response = $response . '<td align="center"> ' . $row['ProjectId'] . ' </td>';
+			$response = $response . '<td align="center"> ' . $row['ProjectName'] . ' </td>';
+			$response = $response . ' </tr>';
+	}
+	$response = $response . '</table>';	
 	
-	//return $userinfo;
-	$result = $userinfo;
 	
+	echo $response;
 	//$id = "1";
 	//$email = "ttc_rul6@hotmail.com";
 	//echo "Before function <BR />";
 	//$result = getUserById($id);
 	//echo "After function <BR />";
 	
-	#test getUserById() 
-	if (!empty($result)) {
-		echo "Results: (TESTING: getUserById())<BR />";
-		echo "<B>FirstName:</B> " . $result['FirstName'] . "<BR />";
-		echo "<B>LastName:</B>  " .  $result['LastName']. "<BR />";
-		echo "<B>Nickname:</B>  " . $result['Nickname']. "<BR />";
-	} else {
-		
-		echo "ID not found <BR />";
-	}
-	
-	echo "<BR /><BR />";
-	
-	
-	/*
-	#test createUser()
-	$fName = "Johnny";
-	$lName = "Depp";
-	$email = "johnny@depp.com";
-	$phone = "12345678";
-	$nick = "Jack";
-	$password = "hi";
-	$type = 1;
-	
-	$result2 = createUser($fName, $lName, $email, $phone, $nick, $password, $type);
-	
-	if (!empty($result2)) {
-		
-		echo "RESULTS 2 (TESTING: createUser()): <BR />";
-		echo "<B> Registered UserID: </B>" . $result2['UserId'] . "<BR />";
-		echo "<B> email: </B>" . $result2['Email'] . "<BR />";
-		echo "<B> Nickname: </B>" . $result2['Nickname'] . "<BR />";
-		echo "<B> Phone: </B>" . $result2['Phone'] . "<BR />";
-	}
-	*/
-	// testing the checkPass()
-	
+
 	//checkPass("hi","johnny@depp.com");
 	
 	
