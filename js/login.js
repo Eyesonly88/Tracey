bfeb974a6be9ebbb47f786e241ec5cbc9f6a1bd9
Adeeb <trah012@aucklanduni.ac.nz>
2011-07-05 02:29:16 +1200

    Some minor fixes. Also added some other loading gif files that could be used if needed. (Using loading.gif for the login menu at the moment).

--- js/login.js
+++ js/login.js
@@ -12,19 +12,6 @@
 
 jQuery(function ($) {
 	
-	
-	$.fn.wait = function(time, type) {
-        time = time || 1000;
-        type = type || "fx";
-        return this.queue(type, function() {
-            var self = this;
-            setTimeout(function() {
-                $(self).dequeue();
-            }, time);
-        });
-    };
-	
-	
 	/* Big variable declaration for the login form */
 	var login = {
 		message: null,
