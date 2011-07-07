ccb4d8283f8475a29b547ec1a62e7315e447abf0
Mohammed <eyesonly88@gmail.com>
2011-07-07 20:06:24 +1200

    refactoring. put the define dashboard script into a separate file.

--- user_dashboard.php
+++ user_dashboard.php
@@ -12,94 +12,17 @@ confirmLogin();
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>User Dashboard</title>
-
+	
+	<link rel="stylesheet" type="text/css" href="css/customStyle.css" />
+    <link rel="stylesheet" type="text/css" href="css/dashboardui.css" />
+    <link rel="stylesheet" type="text/css" href="libraries/dashboard/themes/default/jquery-ui-1.8.2.custom.css" />
     <script type="text/javascript" src="libraries/dashboard/lib/jquery-1.4.2.min.js"></script>
     <script type="text/javascript" src="libraries/dashboard/lib/jquery-ui-1.8.2.custom.min.js"></script>
     <script type="text/javascript" src="libraries/dashboard/jquery.dashboard.js"></script>
     <script type="text/javascript" src="libraries/dashboard/lib/themeroller.js"></script>
 	<script src="js/jquery.hoverIntent.js"></script>
 	<script src="js/loginpanel.js"></script>
-    <script type="text/javascript">
-      // This is the code for definining the dashboard
-      $(document).ready(function() {
-
-        // load the templates
-        $('body').append('<div id="templates"></div>');
-        $("#templates").hide();
-        $("#templates").load("layouts/user_dash_placeholder.html", initDashboard);
-
-        // call for the themeswitcher
-        $('#switcher').themeswitcher();
-
-        // call for the minimal dashboard
-        function initDashboard() {
-          var startId = 100;
-          var dashboard = $('#dashboard').dashboard({
-          	
-          	layoutClass:'layout',
-          	
-            json_data : { url: "/widgets/jsonfeed/defaultwidgets.json" },
-            
-            addWidgetSettings: {
-              widgetDirectoryUrl:"/widgets/jsonfeed/widgetcategories.json"
-            },
-
-            // Definition of the layout
-            // When using the layoutClass, it is possible to change layout using only another class. In this case
-            // you don't need the html property in the layout
-
-            layouts :
-              [
-                { title: "Layout1",
-                  id: "layout1",
-                  image: "/layouts/layout1.png",
-                  html: '<div class="layout layout-a"><div class="column first column-first"></div></div>',
-                  classname: 'layout-a'
-                },
-                { title: "Layout2",
-                  id: "layout2",
-                  image: "/layouts/layout2.png",
-                  html: '<div class="layout layout-aa"><div class="column first column-first"></div><div class="column second column-second"></div></div>',
-                  classname: 'layout-aa'
-                },
-                { title: "Layout3",
-                  id: "layout3",
-                  image: "/layouts/layout3.png",
-                  html: '<div class="layout layout-ba"><div class="column first column-first"></div><div class="column second column-second"></div></div>',
-                  classname: 'layout-ba'
-                },
-                { title: "Layout4",
-                  id: "layout4",
-                  image: "/layouts/layout4.png",
-                  html: '<div class="layout layout-ab"><div class="column first column-first"></div><div class="column second column-second"></div></div>',
-                  classname: 'layout-ab'
-                },
-                { title: "Layout5",
-                  id: "layout5",
-                  image: "/layouts/layout5.png",
-                  html: '<div class="layout layout-aaa"><div class="column first column-first"></div><div class="column second column-second"></div><div class="column third column-third"></div></div>',
-                  classname: 'layout-aaa'
-                }
-              ]   
-            
-          });
-          
-          // binding for a widgets is added to the dashboard
-          dashboard.element.live('dashboardAddWidget',function(e, obj){
-            var widget = obj.widget;
-
-            dashboard.addWidget({
-              "id":startId++,
-              "title":widget.title,
-              "url":widget.url,
-              "metadata":widget.metadata
-              }, dashboard.element.find('.column:first'));
-          });
-          dashboard.init();
-        }
-      });
-
-    </script>
+	<script src="js/definedashboard.js"></script>
     <script>
     $(document).ready(function() {
     	$(".notifications-form").hide();
@@ -114,11 +37,7 @@ confirmLogin();
 		)
 		
 		});
-		
     </script>
-	<link rel="stylesheet" type="text/css" href="css/customStyle.css" />
-    <link rel="stylesheet" type="text/css" href="css/dashboardui.css" />
-    <link rel="stylesheet" type="text/css" href="libraries/dashboard/themes/default/jquery-ui-1.8.2.custom.css" />
 
   </head>
 
