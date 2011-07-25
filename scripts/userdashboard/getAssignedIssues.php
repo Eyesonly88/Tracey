0a13c1856f8941f38f271df54566fae13b0acbdc
Adeeb <trah012@aucklanduni.ac.nz>
2011-07-25 15:29:09 +1200

    Created the userissues.php widget that lists the issues assigned to a user on the user dashbaord.

--- scripts/userdashboard/getAssignedIssues.php
+++ scripts/userdashboard/getAssignedIssues.php
@@ -1,15 +1,50 @@
 <?php 
-include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_issuefunctions.php');
+
+	include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_projectfunctions.php');
+	include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_issuefunctions.php');
+	include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_userfunctions.php');
+	
  /**
  * Returns a JSON string containing the issueId's assigned to a user.
  * The script expects a GET request from the caller with an email parameter set to be the user's email.
  * @TESTED: OK
  */
-	if(isset($_GET['email'])){
-		$emailadd = $_GET['email'];
-		$results = getAssignedIssuesByEmail($emailadd);
-		$jsonResult = json_encode($results);
-		echo $jsonResult;
+
+ 	$result = '';
+	$response = '<thead><tr><th>Issue</th><th>Component</th><th>Reporter</th><th>Assignee</th><th>Type</th><th>Priority</th><th>Status</th><th>Link</th></tr></thead>';
+	if(isset($_POST['email'])){
+		$emailadd = $_POST['email'];
+		$result = getAssignedIssuesByEmail($emailadd);
+		//$jsonResult = json_encode($results);
+		//echo $jsonResult;
 	}
+	
+	if (!(empty($result))) {
+		$response = '';
+		//$response = '<table class="projlistflex"  border="1" cellpadding="3" cellspacing="0">';
+
+		$response = $response . '<thead><tr><th>Issue</th><th>Component</th><th>Reporter</th><th>Assignee</th><th>Type</th><th>Priority</th><th>Status</th><th>Link</th></tr></thead>';
+		$response = $response . '<tbody>';	
+		foreach($result as $row){
+			$reporterEmail = getUserInfoById($row['ReporterId'], "Email");
+			$assigneeEmail = getUserInfoById($row['AssigneeId'], "Email");
+			$response = $response . '<tr>';
+			$response = $response . '<td class="i_id" name="p_id" align="center">' . $row['IssueId'] . '</td>';
+			$response = $response . '<td class="i_component" name="i_component" align="center">' . $row['ComponentId'] . '</td>';
+			$response = $response . '<td class="i_reporter" name="i_reporter" align="center">' . $reporterEmail . '</td>';
+			$response = $response . '<td class="i_assignee" name="i_assignee" align="center">' . $assigneeEmail . '</td>';
+			$response = $response . '<td class="i_type" name="i_type" align="center">' . $row['IssueType'] . '</td>';
+			$response = $response . '<td class="i_priority" name="i_priority" align="center">' . $row['Priority'] . '</td>';
+			$response = $response . '<td class="i_status" name="i_status" align="center">' . $row['IssueStatus'] . '</td>';
+			$response = $response . '<td class="i_viewissue" name="i_viewissue_button" id="view_issue_' . $row['AssigneeId'] . '" align="center"><a href="#"> View Issue </a></td>';
+			$response = $response . '</tr>';
+		}
+		$response = $response . '</tbody>';		
+	} 
+
+	echo $response;
+	
+	
+	
 
 ?>
--- widgets/userdashboard_widgets/userissues.php
+++ widgets/userdashboard_widgets/userissues.php
@@ -6,11 +6,93 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sessions.php');
 
 
 ?>
-
+<link rel="stylesheet" href="/libraries/datatables/media/css/demo_table_jui.css" type="text/css">
+<!--<script src="/libraries/flexigrid/js/flexigrid.js" type="text/javascript"></script> -->
+<script src="/libraries/dataTables/media/js/jquery.dataTables.js" type="text/javascript"></script>
 <script type="text/javascript">
   
+ 
+  var currentUser = "";
+  getCurrentUser();
+  initialSetup();
+  
+  function setUser(msg){ 
+  	currentUser = msg;	
+  }
+  
+  function setupFlexTable(){
+  	
+  	/* Apply datatable library to the list of tables. */
+  	$('#issuelist').dataTable({
+  		
+  		"bPaginate": false,
+		"bLengthChange": false,
+		"bFilter": true,
+		"bSort": false,
+		"bInfo": false,
+		"bAutoWidth": false,
+		"bStateSave": true, 
+		"bDestroy": true,
+		"oSearch": {"sSearch": ""}
+  	
+  	});
+  	//$("#projectlist").fnDraw();
+  }
+  
+  /* Creates a project (via Ajax) */
+ 
 
-  });
+  
+  
+  /* Get the currently logged in user */
+  function getCurrentUser(){
+  	$.ajax({
+  	   cache: "false",
+	   type: "POST",
+	   url: "/scripts/authentication/getCurrentUser.php",
+	   success: function(msg){
+	     setUser(msg);
+	   }
+   
+ 	});
+  }
+  
+  /* This is the initial function call that is executed when the widget is ready. 
+ 	Sets up the initial list of projects. */
+  function initUserIssues(email){
+  
+  $.ajax({
+  	   cache: "false",
+	   type: "POST",
+	   url: "/scripts/userdashboard/getAssignedIssues.php",
+	   data: "email="+email,
+	   success: function(msg2){
+	  
+	     $("#issuelist").empty();
+	     $("#issuelist").append(msg2);
+	     setupFlexTable();
+	     
+	   }
+   
+ 	});
+  }
+  
+  /* The first initial function call, which calls 2 other 
+ 	functions to set up the initial state of the widget*/
+  function initialSetup() {
+  $.ajax({
+  	   cache: "false",
+	   type: "POST",
+	   url: "/scripts/authentication/getCurrentUser.php",
+	   success: function(msg){
+	     setUser(msg);
+	     initUserIssues(msg);
+	    
+	   }
+   
+ 	});
+ }
+  
 
 </script>
 
@@ -48,5 +130,6 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sessions.php');
 </style>
 
 <div id="contents"><h4>Issues:</h4>
-</div>
 
+	<table id="issuelist" class="display" ></table>
+</div>
