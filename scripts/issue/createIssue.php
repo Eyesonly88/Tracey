54dd348e9374f9922db56ddc343c2e67e540812b
Adeeb <trah012@aucklanduni.ac.nz>
2011-08-13 15:12:13 +1200

    Adding templates for the createIssue and modifyIssue callback scripts.

--- scripts/issue/createIssue.php
+++ scripts/issue/createIssue.php
@@ -1,1 +1,40 @@
-<?php ?>
+<?php 
+
+include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_issuefunctions.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_userfunctions.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_projectfunctions.php');
+
+$issueid = '';
+$reporter = '';
+$assignee = '';
+$issuetype =  '';
+$issuestatus = '';
+$priority = '';
+
+
+if (isset($_POST['id'])) {
+	$issueid = $_POST['id'];
+}
+
+if (isset($_POST['reporter'])) {
+	$reporter = $_POST['reporter'];
+}
+
+if (isset($_POST['assignee'])) {
+	$assignee = $_POST['assignee'];		
+}
+
+if (isset($_POST['type'])) {
+	$issuetype = $_POST['type'];
+}
+
+if (isset($_POST['status'])) {
+	$issuestatus = $_POST['status'];
+}
+
+if (isset($_POST['priority'])) {
+	$priority = $_POST['priority'];	
+}
+
+
+?>
--- scripts/issue/modifyIssue.php
+++ scripts/issue/modifyIssue.php
@@ -1,5 +1,40 @@
 <?php 
 
 // Call-back script that accepts ajax POST requests, updates the information in a specified issue.
+include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_issuefunctions.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_userfunctions.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/scripts/includes/sql_projectfunctions.php');
+
+$issueid = '';
+$reporter = '';
+$assignee = '';
+$issuetype =  '';
+$issuestatus = '';
+$priority = '';
+
+
+if (isset($_POST['id'])) {
+	$issueid = $_POST['id'];
+}
+
+if (isset($_POST['reporter'])) {
+	$reporter = $_POST['reporter'];
+}
+
+if (isset($_POST['assignee'])) {
+	$assignee = $_POST['assignee'];		
+}
+
+if (isset($_POST['type'])) {
+	$issuetype = $_POST['type'];
+}
+
+if (isset($_POST['status'])) {
+	$issuestatus = $_POST['status'];
+}
+
+if (isset($_POST['priority'])) {
+	$priority = $_POST['priority'];	
+}
 
 ?>
